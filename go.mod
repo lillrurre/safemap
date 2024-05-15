1a67e13ee38b11c695c56dc3fbf164790447008f
Erik Ehrström <erik.ehrstrom@gmail.com>
2024-05-15 13:15:41 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lillrurre/safemap
+
+go 1.22.2
---
+++ map.go
@@ -0,0 +1,55 @@
+package safemap
+
+import "sync"
+
+type SafeMap[T comparable, V any] struct {
+	mut sync.RWMutex
+	m   map[T]V
+}
+
+func NewMap[T comparable, V any]() *SafeMap[T, V] {
+	return &SafeMap[T, V]{m: make(map[T]V)}
+}
+
+func (s *SafeMap[T, V]) Store(key T, value V) {
+	s.mut.Lock()
+	defer s.mut.Unlock()
+	s.m[key] = value
+}
+
+func (s *SafeMap[T, V]) Delete(key T) {
+	s.mut.Lock()
+	defer s.mut.Unlock()
+	delete(s.m, key)
+}
+
+func (s *SafeMap[T, V]) Load(key T) (value V) {
+	s.mut.RLock()
+	defer s.mut.RUnlock()
+	return s.m[key]
+}
+
+func (s *SafeMap[T, V]) LoadBool(key T) (value V, ok bool) {
+	s.mut.RLock()
+	defer s.mut.RUnlock()
+	value, ok = s.m[key]
+	return value, ok
+}
+
+func (s *SafeMap[T, V]) Range(f func(T, V) bool) {
+	s.mut.RLock()
+	defer s.mut.RUnlock()
+	for k, v := range s.m {
+		if !f(k, v) {
+			break
+		}
+	}
+}
+
+func (s *SafeMap[T, V]) Swap(key any, value any) (previous any, loaded bool) {
+	s.mut.Lock()
+	defer s.mut.Unlock()
+	previous, loaded = s.m[key]
+	s.m[key] = value
+	return previous, loaded
+}
